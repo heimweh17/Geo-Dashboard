b4d67c0bccc51723714c1c91c2b97879c7e6d66a
heimweh17 <71220635+heimweh17@users.noreply.github.com>
2025-11-26 21:27:52 -0500

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,10 @@ import App from './App.jsx';
 import './index.css';
 import { AuthProvider } from './contexts/AuthContext';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const rootElement = document.getElementById('root');
+if (!rootElement) throw new Error('Failed to find the root element');
+
+ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
     <AuthProvider>
       <App />
